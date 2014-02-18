730809f27534023c32aa2900646385c6117aecf4
franspaco <surcouf87@hotmail.com>
2014-02-18 10:20:19 -0600

    First!
    Pretty little program to test command based

---
+++ src/org/tamankeet/frc3933/subsystems/Ultrasonic.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tamankeet.frc3933.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.tamankeet.frc3933.RobotMap;
+import org.tamankeet.frc3933.commands.returnUltrasonic;
+
+/**
+ *
+ * @author PACO
+ */
+public class Ultrasonic extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new returnUltrasonic());
+    }
+    
+    public double getDistance(){
+        double distance = RobotMap.ultraSensor.getAverageVoltage()*206.47;
+        return distance;
+    }
+}
